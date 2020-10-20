bcf68a255203cb0769f017785ad44b976af694f9
Ellen <58341868+Ellenyz@users.noreply.github.com>
2020-10-20 17:25:40 -0500

    data

--- Main Proj.R
+++ Main Proj.R
@@ -6,22 +6,28 @@ rm(list=ls())
 install.packages('ipumsr')
 suppressMessages(library('ipumsr'))
 suppressMessages(library(data.table))
+suppressMessages(library(tidyverse))
 suppressMessages(library(dplyr))
+suppressMessages(library(tidyr))
 suppressMessages(library(ggplot2))
-ddi <- read_ipums_ddi("cps_00002.xml")
+ddi <- read_ipums_ddi("cps_00003.xml")
+setwd('/Users/ellenyz/Documents/GitHub/Ellen')
 data <- read_ipums_micro(ddi)
+
 
 #################
 # Data Cleaning #
 #################
 dt <- data[which(data$MONTH<9),]
-dt <- dt[,c(-1,-3,-4,-5,-6,-7,-10)]
-as.character(dt$CPSIDP)
+as.character(dt$WTFINL)
 factor(dt$SEX)
 factor(dt$WHYUNEMP)
 factor(dt$WHYABSNT)
 factor(dt$WHYPTLWK)
 factor(dt$EMPSAME)
+factor(dt$COUNTY)
+factor(dt$STATEFIP)
+write.csv(dt,file = 'panel_data.csv')
 dt <- dt[is.na(dt$WTFINL)==F,] ##not sure whether this step is proper
 
 # Check for the missing data
@@ -37,6 +43,8 @@ dt[which(dt$WHYUNEMP==0),'WHYUNEMP'] <- NA
 dt[which(dt$WHYABSNT==0),'WHYABSNT'] <- NA
 dt[which(dt$WHYPTLWK==0),'WHYPTLWK'] <- NA
 
+write.csv(dt,file = 'panel_data_cleared.csv')
+
 # Descriptive Analysis
 dt <- data.table(dt)
 
@@ -46,36 +54,83 @@ WH <- dt[dt$MARST==1 & dt$NCHILD>0 & dt$YNGCH<=18,]
   WH$EMP <- NA
   WH$EMP[which(WH$LABFORCE==2 & (WH$EMPSTAT==20|WH$EMPSTAT==21|WH$EMPSTAT==22))] <- 1
   WH$EMP[which(WH$LABFORCE==2 & (WH$EMPSTAT==10|WH$EMPSTAT==12))] <- 0
-  
+
+# Label gender
+WH$SEX <- factor(WH$SEX,levels = c(1,2), labels = c("Male", "Female"))
 
 ##Aggregate by gender
- WH_agg = WH[,.(avg_wh = weighted.mean(AHRSWORKT,WTFINL,na.rm = T), 
+WH_agg = WH[,.(avg_wh = weighted.mean(AHRSWORKT,WTFINL,na.rm = T), 
             unemp_rt = weighted.mean(EMP,WTFINL,na.rm = T), 
             avg_dur_ump = weighted.mean(DURUNEMP,WTFINL,na.rm = T),
             obs = .N), 
             by = .(SEX,MONTH)]
- 
+
+
 #Plot
 #### Average working hours variation through month 1~9 
 #### SampleGroup: Married Couple (Married, spouse present, with child) in labor force
 plt1 <-  ggplot(data=WH_agg,aes(x=MONTH, y=avg_wh, group=factor(SEX), shape=factor(SEX), colour=factor(SEX))) + 
          ggtitle("Average Working Hours") + 
          xlab("Month") + ylab("Hours") + geom_line() + geom_point() + ylim(20,50)
-plt1 + scale_colour_discrete(name  ="Gender",
-                            breaks=c(2, 1),
-                            labels=c("Woman", "Man")) + 
-         scale_shape_discrete(name  ="Gender",
-                       breaks=c(2, 1),
-                       labels=c("Woman", "Man"))
+plt1 + scale_colour_discrete(name  ="Gender", breaks=c(1, 2),labels=c("Man", "Woman")) + 
+         scale_shape_discrete(name  ="Gender",breaks=c(1, 2),labels=c("Man", "Woman")) 
+  
    
 plt2 <-  ggplot(data=WH_agg,aes(x=MONTH, y=unemp_rt, group=factor(SEX), shape=factor(SEX), colour=factor(SEX))) + 
   ggtitle("Umemployment Rate From January to August") + 
   xlab("Month") + ylab("Unemployment") + geom_line() + geom_point()
 plt2 + scale_colour_discrete(name  ="Gender",
-                             breaks=c(2, 1),
-                             labels=c("Woman", "Man")) + 
+                             breaks=c(1, 2),
+                             labels=c("Man", "Woman")) + 
   scale_shape_discrete(name  ="Gender",
-                       breaks=c(2, 1),
-                       labels=c("Woman", "Man"))
+                       breaks=c(1, 2),
+                       labels=c("Man", "Woman"))
 
-   
+
+### Add Relative WH
+WH <- WH[WH$UHRSWORKT!=0,]
+WH$Relative_wh <- WH$AHRSWORKT/WH$UHRSWORKT
+
+Relative_WH_agg = WH[,.(avg_Relative_wh= weighted.mean(Relative_wh,WTFINL,na.rm = T),
+               obs = .N), 
+            by = .(SEX,MONTH)]
+
+
+plt3 <- ggplot(data=Relative_WH_agg, aes(x=MONTH, y=avg_Relative_wh, group=factor(SEX), shape=factor(SEX), colour=factor(SEX))) + 
+  ggtitle("Average Relative Working Hours") + 
+  xlab("Month") + ylab("Working Hour Growth") + geom_line()+ geom_point() + geom_smooth(method='lm', linetype=4, size=0.5, se=FALSE) 
+plt3 + scale_colour_discrete(name  ="Gender",
+                             breaks=c(1,2),
+                             labels=c("Man", "Woman")) + 
+  scale_shape_discrete(name  ="Gender",
+                       breaks=c(1, 2),
+                       labels=c("Man", "Woman")) 
+
+# Check for the distribution of samples among the months
+p <- ggplot(data = WH, mapping = aes(x = MONTH))
+p+geom_bar()
+
+# Unemployment Duration
+plt4 <-  ggplot(data=WH_agg,aes(x=MONTH, y=avg_dur_ump, group=factor(SEX), shape=factor(SEX), colour=factor(SEX))) + 
+  ggtitle("Average Umemployment Duration From January to August") + 
+  xlab("Month") + ylab("Unemployment Duration") + geom_line() + geom_point()
+plt4 + scale_colour_discrete(name  ="Gender",
+                             breaks=c(1, 2),
+                             labels=c("Man", "Woman")) + 
+  scale_shape_discrete(name  ="Gender",
+                       breaks=c(1, 2),
+                       labels=c("Man", "Woman"))
+
+## Descriptive analysis
+table(WH$MONTH,WH$SEX)
+WH_clean <- WH %>%
+  dplyr::select(SEX,MONTH,YNGCH,AHRSWORKT,EMP) %>%
+  group_by(SEX,MONTH) %>%
+  summarize(n = n(),mean_WH = mean(AHRSWORKT,na.rm = TRUE),sd_WH=sd(AHRSWORKT,na.rm = TRUE))
+
+view(WH_clean)
+
+write.csv(WH_clean,file='WH_clean.csv')  
+  
+  
+  
