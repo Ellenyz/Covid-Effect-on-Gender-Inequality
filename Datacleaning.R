1bba89337e27a1919b2b509e7e49c1b6fb5f7eed
Ellen <58341868+Ellenyz@users.noreply.github.com>
2020-10-22 16:13:16 -0500

    load data set

--- Datacleaning.R
+++ Datacleaning.R
@@ -28,3 +28,16 @@ factor(dt$COUNTY)
 factor(dt$STATEFIP)
 write.csv(dt,file = 'panel_data.csv')
 dt <- dt[is.na(dt$WTFINL)==F,] ##not sure whether this step is proper
+
+# Check for the missing data
+dt[which(dt$AHRSWORKT==999),'AHRSWORKT'] <- NA # Hours Worked Lase Week
+dt[which(dt$UHRSWORKT==999),'UHRSWORKT'] <- NA # Usual Working Hours
+dt[which(dt$ABSENT==0),'ABSENT'] <- NA # Dummy_Absent from work
+dt[which(dt$DURUNEMP==999),'DURUNEMP'] <- NA # Duration of Unemp
+dt[which(dt$EMPSAME==99),'EMPSAME'] <- NA # Whether change employers
+dt[which(dt$SEX==9),'SEX'] <- NA  # 1:Male 2: Female
+dt[which(dt$EMPSTAT==0),'EMPSTAT'] <- NA
+dt[which(dt$ABSENT==0),'ABSENT'] <- NA
+dt[which(dt$WHYUNEMP==0),'WHYUNEMP'] <- NA
+dt[which(dt$WHYABSNT==0),'WHYABSNT'] <- NA
+dt[which(dt$WHYPTLWK==0),'WHYPTLWK'] <- NA
